1f531233bdc5bdd92be812ccf3f085eb1d765184
dzone24 <derrick.munkhbat@gmail.com>
2024-04-29 13:41:05 +0800

    ...

--- pages/index.js
+++ pages/index.js
@@ -7,12 +7,12 @@ export default function Home() {
       <div className="absolute content min-w-full flex flex-col h-screen overflow-y-scroll scroll-smooth snap-y snap-mandatory lg:max-lg:w-96 mx-auto">
         {/* <Header /> */}
         <div className="snap-start flex w-full h-44 border-y-2 justify-evenly gap-6 px-8 py-4 items-center">
-          <div className="text-white text-2xl font-bold text-center">
+          <div className="text-secondary text-2xl font-bold text-center">
             ALPHA TRIANGLE
           </div>
 
           {/* <MainMenu items={menuItems} /> */}
-          <div className="hidden lg:flex w-full justify-evenly font-bold text-white text-2xl">
+          <div className="hidden lg:flex w-full justify-evenly font-bold text-secondary text-2xl">
             <h1>WELCOME</h1>
             <h1>WHO WE ARE</h1>
             <h1>WHAT WE DO</h1>
